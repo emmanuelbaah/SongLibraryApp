0423855d223be4d79eebcc32ae69494643a87b75
Arpit Shah <arpitshah101@gmail.com>
2018-03-01 21:35:41 -0500

    Create draft of Splash page

--- src/public/app/app-routing.module.ts
+++ src/public/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { WelcomeComponent } from './welcome/welcome.component';
+
 const routes: Routes = [
   {
     path: '',
-    children: []
+    pathMatch: 'full',
+    redirectTo: '/welcome',
+  },
+  {
+    path: 'welcome',
+    component: WelcomeComponent,
   }
 ];
 
