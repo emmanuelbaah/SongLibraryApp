b4f0e3d4100939bcd2bfda128de25192c867a2db
Arpit Shah <arpitshah101@users.noreply.github.com>
2018-03-09 23:04:31 -0500

    JSON Token-based Authentication API (#5)
    * Add necessary node packages for authentication
    
    * Add object literal ordering rule for TSLint
    
    * Prototype code for authentication system using JWT
    
    * Disable linting for console outputs throughout development
    
    * Add 'bcrypt' as dependency for password encryption
    
    * Add code for registering user and creating new corresponding library
    
    * Graceful shutdown code for application
    
    When SIGINT is received, code should intercept and close
    default mongoose db connection.

---
+++ src/modules/authentication.ts
@@ -0,0 +1,79 @@
+import * as bcrypt from 'bcrypt';
+import * as Bluebird from 'bluebird';
+import * as mongoose from 'mongoose';
+
+// Just to make the types work out nicely since we use Bluebird as the Promise library ;)
+declare module 'bcrypt' {
+    type Promise<T> = Bluebird<T>;
+}
+
+import { ILibraryDocument, IUserDocument, UserModel, LibraryModel } from '../models';
+
+export class AuthenticationModule {
+
+    // # of rounds needed to encrypt. increase to double security. decrease to halve it.
+    private static saltRounds = 10;
+
+    /**
+     * registerUser - Attempts to create a new user account with an empty library for new user
+     *
+     * @static
+     * @param {{username: string, password: string, name: string, email: string}} user object with needed user information
+     * @returns {Bluebird<IUserDocument>} Promise of saved User document
+     * @memberof AuthenticationModule
+     */
+    public static registerUser(user: {username: string, password: string, name: string, email: string}): Bluebird<IUserDocument> {
+        // TODO: ERROR CHECKING!!
+        let newUser = new UserModel(user);
+
+        return bcrypt.hash(newUser.password, AuthenticationModule.saltRounds)
+            .then(encryptedPassword => {
+                newUser.password = encryptedPassword;
+            })
+            .then(() => newUser.save())
+            .catch(error => {
+                // Check for any error creating user
+                throw error;
+            })
+            .then((userDoc: IUserDocument) => {
+                // set aside new user doc for lib reference update & create new lib doc
+                newUser = userDoc;
+                const newLib = new LibraryModel();
+                return newLib.save();
+            })
+            .then((libDoc: ILibraryDocument) => {
+                // Assuming lib doc is created, link it in User doc & update user doc
+                newUser.library = mongoose.Types.ObjectId(libDoc._id);
+                return newUser.save();
+            });
+    }
+
+    /**
+     * verifyUser - Verify that user exists with matching username and password.
+     * Throws error if no user found or if password is incorrect.
+     *
+     * @static
+     * @param {string} username Username for user to query
+     * @param {string} password Entered password to verify
+     * @returns {Bluebird<IUserDocument>} User document from database
+     * @memberof AuthenticationModule
+     */
+    public static verifyUser(username: string, password: string): Bluebird<IUserDocument> {
+        let userDoc;
+        return UserModel.findOne({ username })
+                        .then(user => {
+                            if (user === null) {
+                                throw new Error(`No user found with username ${username}`);
+                            }
+                            userDoc = user;
+                            return user;
+                        })
+                        .then(user => bcrypt.compare(password, user.password))
+                        .then(matches => {
+                            if (!matches) {
+                                throw new Error(`Invalid password.`);
+                            }
+                            return userDoc;
+                        });
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,25 @@ import * as bodyParser from 'body-parser';
 
 import * as readline from 'readline';
 
-import { TestRoutes } from './routes';
+import { AuthRoutes, TestRoutes } from './routes';
 
-// import { TestRoutes } from './routes';
+import * as mongoose from 'mongoose';
+
+import * as Bluebird from 'bluebird';
+
+// Set mongoose' default promise engine to Bluebird
+(<any>mongoose).Promise = Bluebird;
+declare module 'mongoose' {
+    type Promise<T> = Bluebird<T>;
+}
+
+mongoose.connect('mongodb://localhost/song-library')
+        .catch(error => {
+            console.error(error);
+            process.exit(1);
+        });
+
+console.debug(`Successfully connected to database!`);
 
 const app = express();
 
@@ -21,7 +37,7 @@ app.use((req: express.Request, res: express.Response, next: () => void) => {
 });
 
 // Add routing to each of the different subroute modules
-app.use('/api/', [TestRoutes, (req: express.Request, res: express.Response) => {
+app.use('/api/', [AuthRoutes, TestRoutes, (req: express.Request, res: express.Response) => {
     // If no API routes are valid, send 404 for not found
     if (!res.headersSent) {
         res.sendStatus(404);
@@ -37,3 +53,24 @@ app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Application running on localhost:${port}`));
+
+// Graceful shutdown portion
+if (process.platform === 'win32') {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on('SIGINT', () => {
+        process.emit('SIGINT');
+    });
+}
+
+process.on('SIGINT', () => {
+    console.log('\nSIGINT detected!');
+    mongoose.connection.close()
+    .then(() => {
+        console.log('\nGracefully shutting down the application.');
+        process.exit();
+    });
+});
